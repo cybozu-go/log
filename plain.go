1607283ca561a6e8f67529626dcad5f276ef9fb2
ymmt2005 <ymmt2005@gmail.com>
2016-08-03 18:50:15 +0900

    Add plain text formatter.

--- plain.go
+++ plain.go
@@ -1,6 +1,12 @@
 package log
 
-import "time"
+import (
+	"fmt"
+	"sort"
+	"strconv"
+	"time"
+	"unicode/utf8"
+)
 
 // PlainFormat implements Formatter to generate plain log messages.
 //
@@ -11,10 +17,170 @@ type PlainFormat struct{}
 // Format implements Formatter.Format.
 func (f PlainFormat) Format(buf []byte, l *Logger, t time.Time, severity int,
 	msg string, fields map[string]interface{}) ([]byte, error) {
-	//var err error
+	var err error
 
 	// assume enough capacity for mandatory fields (except for msg).
-	buf = t.UTC().AppendFormat(buf, time.RFC3339Nano)
+	buf = t.UTC().AppendFormat(buf, RFC3339Micro)
+	buf = append(buf, ' ')
+	buf = append(buf, utsname...)
+	buf = append(buf, ' ')
+	buf = append(buf, l.Topic()...)
+	buf = append(buf, ' ')
+	if ss, ok := severityMap[severity]; ok {
+		buf = append(buf, ss...)
+	} else {
+		buf = strconv.AppendInt(buf, int64(severity), 10)
+	}
+	buf = append(buf, ": "...)
+	buf, err = appendPlain(buf, msg)
+	if err != nil {
+		return nil, err
+	}
 
-	return append(buf, byte('\n')), nil
+	if len(fields) > 0 {
+		keys := make([]string, 0, len(fields))
+		for k := range fields {
+			if !IsValidKey(k) {
+				return nil, ErrInvalidKey
+			}
+			keys = append(keys, k)
+		}
+		sort.Strings(keys)
+		for _, k := range keys {
+			if cap(buf) < (len(k) + 2) {
+				return nil, ErrTooLarge
+			}
+			buf = append(buf, ' ')
+			buf = append(buf, k...)
+			buf = append(buf, '=')
+			buf, err = appendPlain(buf, fields[k])
+			if err != nil {
+				return nil, err
+			}
+		}
+	}
+
+	for k, v := range l.Defaults() {
+		if _, ok := fields[k]; ok {
+			continue
+		}
+		if cap(buf) < (len(k) + 2) {
+			return nil, ErrTooLarge
+		}
+		buf = append(buf, ' ')
+		buf = append(buf, k...)
+		buf = append(buf, '=')
+		buf, err = appendPlain(buf, v)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	if cap(buf) < 1 {
+		return nil, ErrTooLarge
+	}
+	return append(buf, '\n'), nil
+}
+
+func appendPlain(buf []byte, v interface{}) ([]byte, error) {
+	switch t := v.(type) {
+	case nil:
+		if cap(buf) < 4 {
+			return nil, ErrTooLarge
+		}
+		return append(buf, "null"...), nil
+	case bool:
+		if cap(buf) < 5 { // len("false")
+			return nil, ErrTooLarge
+		}
+		return strconv.AppendBool(buf, t), nil
+	case time.Time:
+		// len("2006-01-02T15:04:05.000000Z07:00")
+		if cap(buf) < 32 {
+			return nil, ErrTooLarge
+		}
+		return t.UTC().AppendFormat(buf, RFC3339Micro), nil
+	case int:
+		// len("-9223372036854775807")
+		if cap(buf) < 20 {
+			return nil, ErrTooLarge
+		}
+		return strconv.AppendInt(buf, int64(t), 10), nil
+	case int8:
+		// len("-128")
+		if cap(buf) < 4 {
+			return nil, ErrTooLarge
+		}
+		return strconv.AppendInt(buf, int64(t), 10), nil
+	case int16:
+		// len("-32768")
+		if cap(buf) < 6 {
+			return nil, ErrTooLarge
+		}
+		return strconv.AppendInt(buf, int64(t), 10), nil
+	case int32:
+		// len("-2147483648")
+		if cap(buf) < 11 {
+			return nil, ErrTooLarge
+		}
+		return strconv.AppendInt(buf, int64(t), 10), nil
+	case int64:
+		// len("-9223372036854775807")
+		if cap(buf) < 20 {
+			return nil, ErrTooLarge
+		}
+		return strconv.AppendInt(buf, t, 10), nil
+	case uint:
+		// len("18446744073709551615")
+		if cap(buf) < 20 {
+			return nil, ErrTooLarge
+		}
+		return strconv.AppendUint(buf, uint64(t), 10), nil
+	case uint8:
+		// len("255")
+		if cap(buf) < 3 {
+			return nil, ErrTooLarge
+		}
+		return strconv.AppendUint(buf, uint64(t), 10), nil
+	case uint16:
+		// len("65535")
+		if cap(buf) < 5 {
+			return nil, ErrTooLarge
+		}
+		return strconv.AppendUint(buf, uint64(t), 10), nil
+	case uint32:
+		// len("4294967295")
+		if cap(buf) < 10 {
+			return nil, ErrTooLarge
+		}
+		return strconv.AppendUint(buf, uint64(t), 10), nil
+	case uint64:
+		// len("18446744073709551615")
+		if cap(buf) < 20 {
+			return nil, ErrTooLarge
+		}
+		return strconv.AppendUint(buf, t, 10), nil
+	case float32:
+		if cap(buf) < 256 {
+			return nil, ErrTooLarge
+		}
+		return strconv.AppendFloat(buf, float64(t), 'f', -1, 32), nil
+	case float64:
+		if cap(buf) < 256 {
+			return nil, ErrTooLarge
+		}
+		return strconv.AppendFloat(buf, t, 'f', -1, 64), nil
+	case string:
+		if !utf8.ValidString(t) {
+			return nil, ErrInvalidData
+		}
+		// escaped length = 2*len(t) + 2 double quotes
+		if cap(buf) < (len(t)*2 + 2) {
+			return nil, ErrTooLarge
+		}
+		return strconv.AppendQuote(buf, t), nil
+	default:
+		// other types are just formatted as string with "%v".
+		return appendPlain(buf, fmt.Sprintf("%v", t))
+	}
 }
---
+++ plain_test.go
@@ -0,0 +1,143 @@
+package log
+
+import (
+	"bytes"
+	"testing"
+	"time"
+)
+
+func TestAppendPlain(t *testing.T) {
+	t.Parallel()
+
+	buf := make([]byte, 0, 4096)
+
+	b, _ := appendPlain(buf, nil)
+	if string(b) != "null" {
+		t.Error(`string(b) != "null"`)
+	}
+
+	b, _ = appendPlain(buf, true)
+	if string(b) != "true" {
+		t.Error(`string(b) != "true"`)
+	}
+
+	b, _ = appendPlain(buf, int16(-12345))
+	if string(b) != "-12345" {
+		t.Error(`string(b) != "-12345"`)
+	}
+
+	b, err := appendPlain(buf, []string{"abc", "def"})
+	if err != nil {
+		t.Error(err)
+	} else {
+		if !bytes.Contains(b, []byte("abc")) {
+			t.Error(`!bytes.Contain(b, "abc")`)
+		}
+		if !bytes.Contains(b, []byte("def")) {
+			t.Error(`!bytes.Contain(b, "def")`)
+		}
+	}
+
+	b, err = appendPlain(buf, map[string]int{
+		"abc": 100,
+		"def": -1,
+	})
+	if err != nil {
+		t.Error(err)
+	} else {
+		if !bytes.Contains(b, []byte("abc")) {
+			t.Error(`!bytes.Contain(b, "abc")`)
+		}
+		if !bytes.Contains(b, []byte("-1")) {
+			t.Error(`!bytes.Contain(b, "-1")`)
+		}
+	}
+}
+
+const (
+	testPlainLog1 = `2001-12-03T13:45:01.123456Z localhost tag1 debug: "test message"` + "\n"
+	testPlainLog2 = `2001-12-03T13:45:01.123456Z localhost tag2 debug: "test message" secret=true` + "\n"
+	testPlainLog3 = `2001-12-03T13:45:01.123456Z localhost tag2 debug: "test message" secret=false` + "\n"
+	testPlainLog4 = `2001-12-03T13:45:01.123456Z localhost tag3 debug: "test message" a=1 b=2 c=3 d=4` + "\n"
+)
+
+func TestPlainFormat1(t *testing.T) {
+	t.Parallel()
+
+	utsname = "localhost"
+
+	l := NewLogger()
+	l.SetTopic("tag1")
+
+	ts := time.Date(2001, time.December, 3, 13, 45, 1, 123456789, time.UTC)
+	f := PlainFormat{}
+	b := make([]byte, 0, 4096)
+
+	if buf, err := f.Format(b, l, ts, LvDebug, "test message", nil); err != nil {
+		t.Error(err)
+	} else {
+		if string(buf) != testPlainLog1 {
+			t.Error(string(buf) + " != " + testPlainLog1)
+		}
+	}
+}
+
+func TestPlainFormat2(t *testing.T) {
+	t.Parallel()
+
+	utsname = "localhost"
+
+	l := NewLogger()
+	l.SetTopic("tag2")
+	l.SetDefaults(map[string]interface{}{FnSecret: true})
+
+	ts := time.Date(2001, time.December, 3, 13, 45, 1, 123456789, time.UTC)
+	f := PlainFormat{}
+	b := make([]byte, 0, 4096)
+
+	if buf, err := f.Format(b, l, ts, LvDebug, "test message", nil); err != nil {
+		t.Error(err)
+	} else {
+		if string(buf) != testPlainLog2 {
+			t.Error(string(buf) + " != " + testPlainLog2)
+		}
+	}
+
+	// override the default
+	fields := map[string]interface{}{
+		FnSecret: false,
+	}
+	if buf, err := f.Format(b, l, ts, LvDebug, "test message", fields); err != nil {
+		t.Error(err)
+	} else {
+		if string(buf) != testPlainLog3 {
+			t.Error(string(buf) + " != " + testPlainLog3)
+		}
+	}
+}
+
+func TestPlainFormat3(t *testing.T) {
+	t.Parallel()
+
+	utsname = "localhost"
+
+	l := NewLogger()
+	l.SetTopic("tag3")
+
+	ts := time.Date(2001, time.December, 3, 22, 45, 1, 123456789,
+		time.FixedZone("Asia/Tokyo", 9*3600))
+	f := PlainFormat{}
+	b := make([]byte, 0, 4096)
+
+	fields := map[string]interface{}{
+		"a": 1, "b": 2, "c": 3, "d": 4,
+	}
+
+	if buf, err := f.Format(b, l, ts, LvDebug, "test message", fields); err != nil {
+		t.Error(err)
+	} else {
+		if string(buf) != testPlainLog4 {
+			t.Error(string(buf) + " != " + testPlainLog4)
+		}
+	}
+}
