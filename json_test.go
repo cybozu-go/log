05351ff3a2b4f0707020ecef70fbbd22a74092fd
itchyny <itchyny@cybozu.co.jp>
2022-03-15 08:54:48 +0900

    Rewrite logger tests into got/want style to omit expectation duplicates

--- json_test.go
+++ json_test.go
@@ -57,48 +57,48 @@ func TestJSONFormat(t *testing.T) {
 	if v, ok := j[FnTopic]; !ok {
 		t.Error(`v, ok := j[FnTopic]; !ok`)
 	} else {
-		if v.(string) != "topic1" {
-			t.Error(`v.(string) != "topic1"`)
+		if got, want := v.(string), "topic1"; got != want {
+			t.Errorf("got %q, want %q", got, want)
 		}
 	}
 
 	if v, ok := j[FnLoggedAt]; !ok {
 		t.Error(`v, ok := j[FnLoggedAt]; !ok`)
 	} else {
-		if v.(string) != "2001-12-03T13:45:01.123456Z" {
-			t.Error(`v.(string) != "2001-12-03T13:45:01.123456Z"`)
+		if got, want := v.(string), "2001-12-03T13:45:01.123456Z"; got != want {
+			t.Errorf("got %q, want %q", got, want)
 		}
 	}
 
 	if v, ok := j[FnUtsname]; !ok {
 		t.Error(`v, ok := j[FnUtsname]; !ok`)
 	} else {
-		if v.(string) != "localhost" {
-			t.Error(`v.(string) != "localhost"`)
+		if got, want := v.(string), "localhost"; got != want {
+			t.Errorf("got %q, want %q", got, want)
 		}
 	}
 
 	if v, ok := j[FnSeverity]; !ok {
 		t.Error(`v, ok := j[FnSeverity]; !ok`)
 	} else {
-		if v.(string) != "critical" {
-			t.Error(`v.(string) != "critical"`)
+		if got, want := v.(string), "critical"; got != want {
+			t.Errorf("got %q, want %q", got, want)
 		}
 	}
 
 	if v, ok := j[FnMessage]; !ok {
 		t.Error(`v, ok := j[FnMessage]; !ok`)
 	} else {
-		if v.(string) != "hoge" {
-			t.Error(`v.(string) != "hoge"`)
+		if got, want := v.(string), "hoge"; got != want {
+			t.Errorf("got %q, want %q", got, want)
 		}
 	}
 
 	if v, ok := j["abc"]; !ok {
 		t.Error(`v, ok := j["abc"]; !ok`)
 	} else {
-		if int(v.(float64)) != 123 {
-			t.Error(`int(v.(float64)) != 123`)
+		if got, want := int(v.(float64)), 123; got != want {
+			t.Errorf("got %q, want %q", got, want)
 		}
 	}
 
@@ -138,43 +138,40 @@ func TestJSONFormat(t *testing.T) {
 	if v, ok := j[FnSeverity]; !ok {
 		t.Error(`v, ok := j[FnSeverity]; !ok`)
 	} else {
-		if v.(string) != "debug" {
-			t.Error(`v.(string) != "debug"`)
+		if got, want := v.(string), "debug"; got != want {
+			t.Errorf("got %q, want %q", got, want)
 		}
 	}
 
 	if v, ok := j[FnMessage]; !ok {
 		t.Error(`v, ok := j[FnMessage]; !ok`)
 	} else {
-		if v.(string) != "fuga fuga" {
-			t.Error(`v.(string) != "fuga fuga"`)
+		if got, want := v.(string), "fuga fuga"; got != want {
+			t.Errorf("got %q, want %q", got, want)
 		}
 	}
 
 	if v, ok := j["abc"]; !ok {
 		t.Error(`v, ok := j["abc"]; !ok`)
 	} else {
-		if !reflect.DeepEqual(v.([]interface{}), []interface{}{1.0, 2.0, 3.0}) {
-			t.Error(`!reflect.DeepEqual(v.([]interface{}), []interface{}{1, 2, 3})`)
-			t.Logf("%#v", v)
+		if got, want := v.([]interface{}), []interface{}{1.0, 2.0, 3.0}; !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v, want %v", got, want)
 		}
 	}
 
 	if v, ok := j["float32"]; !ok {
 		t.Error(`v, ok := j["float32"]; !ok`)
 	} else {
-		if !reflect.DeepEqual(v.([]interface{}), []interface{}{3.14159, "NaN", "+Inf", "-Inf"}) {
-			t.Error(`!reflect.DeepEqual(v.([]interface{}), []interface{}{3.14159, "NaN", "+Inf", "-Inf"})`)
-			t.Logf("%#v", v)
+		if got, want := v.([]interface{}), []interface{}{3.14159, "NaN", "+Inf", "-Inf"}; !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v, want %v", got, want)
 		}
 	}
 
 	if v, ok := j["float64"]; !ok {
 		t.Error(`v, ok := j["float64"]; !ok`)
 	} else {
-		if !reflect.DeepEqual(v.([]interface{}), []interface{}{3.14159, "NaN", "+Inf", "-Inf"}) {
-			t.Error(`!reflect.DeepEqual(v.([]interface{}), []interface{}{3.14159, "NaN", "+Inf", "-Inf"})`)
-			t.Logf("%#v", v)
+		if got, want := v.([]interface{}), []interface{}{3.14159, "NaN", "+Inf", "-Inf"}; !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v, want %v", got, want)
 		}
 	}
 
@@ -189,32 +186,32 @@ func TestJSONFormat(t *testing.T) {
 	if v, ok := j["invalid"]; !ok {
 		t.Error(`v, ok := j["invalid"]; !ok`)
 	} else {
-		if v.(string) != "12\uFFFD34" {
-			t.Error(`v.(string) != "12\uFFFD34"`)
+		if got, want := v.(string), "12\uFFFD34"; got != want {
+			t.Errorf("got %q, want %q", got, want)
 		}
 	}
 
 	if v, ok := j["tm"]; !ok {
 		t.Error(`v, ok := j["tm"]; !ok`)
 	} else {
-		if v.(string) != `a", b, c` {
-			t.Error("v.(string) != `a\", b, c`")
+		if got, want := v.(string), `a", b, c`; got != want {
+			t.Errorf("got %q, want %q", got, want)
 		}
 	}
 
 	if v, ok := j["jm"]; !ok {
 		t.Error(`v, ok := j["jm"]; !ok`)
 	} else {
-		if v.(string) != `a", b, c` {
-			t.Error("v.(string) != `a\", b, c`")
+		if got, want := v.(string), `a", b, c`; got != want {
+			t.Errorf("got %q, want %q", got, want)
 		}
 	}
 
 	if v, ok := j["err"]; !ok {
 		t.Error(`v, ok := j["err"]; !ok`)
 	} else {
-		if v.(string) != `a", b, c` {
-			t.Error("v.(string) != `a\", b, c`")
+		if got, want := v.(string), `a", b, c`; got != want {
+			t.Errorf("got %q, want %q", got, want)
 		}
 	}
 }
